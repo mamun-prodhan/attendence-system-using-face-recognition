334cbcdf41fff3fedfc0b667337d8b0ac49e742a
mamun-prodhan <mamunpr9@gmail.com>
2022-11-12 00:48:17 +0600

    excel file added

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 from student import Student
 from train import Train
 from face_recognition import Face_Recognition
+from attendance import Attendance
 
 
 class Face_Recognition_System:
@@ -73,15 +74,15 @@ class Face_Recognition_System:
         b1_1.place(x=500,y=300, width=220,height=40)
 
 
-        #Attendance Button
+        #Attendance face Button
         img6=Image.open(r"college_images\attendance.jpg")
         img6=img6.resize((220,220),Image.ANTIALIAS)
         self.photoimg6=ImageTk.PhotoImage(img6)
 
-        b1=Button(bg_img,image=self.photoimg6,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg6,cursor="hand2",command=self.attendance_data)
         b1.place(x=800,y=100, width=220,height=220)
 
-        b1_1=Button(bg_img,text="Attendance",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1=Button(bg_img,text="Attendance",cursor="hand2",command=self.attendance_data,font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=800,y=300, width=220,height=40)
 
 
@@ -161,6 +162,14 @@ class Face_Recognition_System:
     def face_data(self):
         self.new_window=Toplevel(self.root)
         self.app=Face_Recognition(self.new_window)
+
+    def attendance_data(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Attendance(self.new_window)
+
+
+
+
 
 
 if __name__ == "__main__":
